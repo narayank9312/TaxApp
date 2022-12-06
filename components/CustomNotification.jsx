09260d35e52771c1858197ffe685f6673cf86421
narayan jha <narayan@opoyi.com>
2022-12-06 21:09:57 +0530

    search implement

--- components/CustomNotification.jsx
+++ components/CustomNotification.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { taxData } from "../utils/taxtable";
 import TopCss from "../styles/CustomNotification.module.css";
 
-const CustomNotification = () => {
+const CustomNotification = ({ searchItems }) => {
   return (
     <div
       style={{
@@ -10,54 +10,82 @@ const CustomNotification = () => {
         width: "71.3rem",
       }}
     >
-      {taxData?.map((data, index) => (
-        <>
-          {index !== 0 && data?.["S.No."] && (
-            <div className={TopCss.divider}></div>
-          )}
-          <div
-            key={index}
-            style={{ marginTop: "3%", width: "71.3rem" }}
-            className={TopCss.topbar_parent_Title}
-          >
-            {/* <span className={TopCss.request_Keywords}>{data?.["S.No."]}</span>
+      {taxData
+        ?.filter((data) => {
+          if (searchItems === "") return data;
+          if (data?.["Description of goods"]) {
+            if (
+              data["Description of goods"]
+                .toLowerCase()
+                .includes(searchItems.toLowerCase())
+            ) {
+              return data;
+            }
+          }
+        })
+        .map((data, index) => (
+          <>
+            {index !== 0 && data?.["S.No."] && (
+              <div className={TopCss.divider}></div>
+            )}
+            <div
+              key={index}
+              style={{ marginTop: "1%", width: "71.3rem" }}
+              className={TopCss.topbar_parent_Title}
+            >
+              {/* <span className={TopCss.request_Keywords}>{data?.["S.No."]}</span>
           <span className={TopCss.request_Keywords}>
             {data?.["Description of goods"]}
           </span> */}
 
-            <span style={{ width: "14%" }} className={TopCss.request_status}>
-              {data?.["S.No."]}
-            </span>
-            <span style={{ width: "14%" }} className={TopCss.request_requested}>
-              {data?.["Chapter or Heading or sub-heading or tariff item"]}
-            </span>
-            <span style={{ width: "14%" }} className={TopCss.request_requested}>
-              {data?.["Description of goods"]}
-            </span>
-            <span style={{ width: "14%" }} className={TopCss.request_resolved}>
-              {data?.["Standard rate"] !== "Nil" &&
-                !isNaN(data?.["Standard rate"] * 100) &&
-                data?.["Standard rate"] * 100 + `%`}
-              {/* {data?.["Standard rate"]} */}
-            </span>
-            <span style={{ width: "14%" }} className={TopCss.request_update}>
-              {data?.["Integrated Goods and Services Tax"]}
-            </span>
-            <span style={{ width: "14%" }} className={TopCss.request_resolved}>
-              {data?.["Amended By Notification No."]}
-            </span>
-            <span style={{ width: "14%" }} className={TopCss.request_resolved}>
-              {data?.["Condition No."]}
-            </span>
-            {/* <span className={TopCss.request_resolved}>
+              <span style={{ width: "14%" }} className={TopCss.request_status}>
+                {data?.["S.No."]}
+              </span>
+              <span
+                style={{ width: "14%" }}
+                className={TopCss.request_requested}
+              >
+                {data?.["Chapter or Heading or sub-heading or tariff item"]}
+              </span>
+              <span
+                style={{ width: "14%" }}
+                className={TopCss.request_requested}
+              >
+                {data?.["Description of goods"]}
+              </span>
+              <span
+                style={{ width: "14%" }}
+                className={TopCss.request_resolved}
+              >
+                {data?.["Standard rate"] !== "Nil" &&
+                  !isNaN(data?.["Standard rate"] * 100) &&
+                  data?.["Standard rate"] * 100 + `%`}
+                {/* {data?.["Standard rate"]} */}
+              </span>
+              <span style={{ width: "14%" }} className={TopCss.request_update}>
+                {data?.["Integrated Goods and Services Tax"]}
+              </span>
+              <span
+                style={{ width: "14%" }}
+                className={TopCss.request_resolved}
+              >
+                {data?.["Amended By Notification No."]}
+              </span>
+              <span
+                style={{ width: "14%" }}
+                className={TopCss.request_resolved}
+              >
+                {data?.["Condition No."]}
+              </span>
+              {/* <span className={TopCss.request_resolved}>
             {data?.["Standard rate"]}
           </span> */}
-            {/* <span className={TopCss.request_resolved}>
+              {/* <span className={TopCss.request_resolved}>
             {data?.["Condition No."]}
           </span> */}
-          </div>
-        </>
-      ))}
+            </div>
+          </>
+        ))}
     </div>
   );
 };
--- pages/TaxRule.jsx
+++ pages/TaxRule.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import CustomNotification from "../components/CustomNotification";
 import TopBar from "../components/TopBar";
 import Request from "../styles/CustomNotification.module.css";
 
 function TaxRule() {
+  const [SearchTerm, setSearchTerm] = useState("");
+
   return (
     <div className={Request.parent}>
+      <input
+        style={{
+          width: "50rem",
+          height: "3rem",
+          marginLeft: "14%",
+          marginBottom: "1%",
+        }}
+        type="text"
+        placeholder={"Search by Description of goods..."}
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
       <TopBar headingTitle={"Request"} />
       {/* {request.length > 0 && <RequestDataList requestData={request} />} */}
-      {<CustomNotification />}
+      {<CustomNotification searchItems={SearchTerm} />}
     </div>
   );
 }
